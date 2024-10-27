d6b2ae4160339e7aa0228ca4f5ac053d832b7517
aynugek <kegunya@gmail.com>
2024-10-27 22:15:25 +0300

    refactor

--- src/main.rs
+++ src/main.rs
@@ -12,35 +12,30 @@ use Operation::*;
 fn get_number() -> f64 {
     loop {
         let mut input = String::new();
-
         io::stdin().read_line(&mut input).unwrap();
 
-        let input: f64 = match input.trim().parse() {
-            Ok(num) => num,
-            Err(_) => {
-                println!("Please type a number!");
-                continue;
-            }
-        };
-        return input;
+        match input.trim().parse::<f64>() {
+            Ok(num) => return num,
+            Err(_) => println!("Only numbers are allowed!"),
+        }
     }
 }
 
-fn get_operation() -> String {
+fn get_operation() -> Operation {
     loop {
-        println!("Enter the operation to be performed: Add, Subtract, Multiply, Divide");
+        println!(
+            "Enter the operation to perform (Add | + | Subtract | - | Multiply | * | Divide | /):"
+        );
         let mut operation = String::new();
-
         io::stdin().read_line(&mut operation).unwrap();
 
-        let operation = match operation.trim() {
-            op @ ("Add" | "Subtract" | "Multiply" | "Divide") => op,
-            _ => {
-                println!("invalid operation");
-                continue;
-            }
-        };
-        return operation.to_string();
+        match operation.to_lowercase().trim() {
+            "add" | "+" => return Add(0.0, 0.0),
+            "subtract" | "-" => return Subtract(0.0, 0.0),
+            "multiply" | "*" => return Multiply(0.0, 0.0),
+            "divide" | "/" => return Divide(0.0, 0.0),
+            _ => (),
+        }
     }
 }
 
@@ -49,27 +44,30 @@ fn calculate(operation: Operation) -> f64 {
         Add(num1, num2) => num1 + num2,
         Subtract(num1, num2) => num1 - num2,
         Multiply(num1, num2) => num1 * num2,
-        Divide(num1, num2) => num1 / num2,
+        Divide(num1, num2) => {
+            if num2 != 0.0 {
+                num1 / num2
+            } else {
+                f64::NAN
+            }
+        }
     }
 }
 
 fn main() {
-    println!("Enter your first number");
+    println!("Enter your first number:");
     let first_num = get_number();
 
-    let operation = get_operation();
+    let mut operation = get_operation();
 
-    println!("Enter your second number");
+    println!("Enter your second number:");
     let second_num = get_number();
 
-    let operation = match operation.as_str() {
-        "Add" => Add(first_num, second_num),
-        "Subtract" => Subtract(first_num, second_num),
-        "Multiply" => Multiply(first_num, second_num),
-        "Divide" => Divide(first_num, second_num),
-        _ => {
-            panic!("Invalid operation");
-        }
+    operation = match operation {
+        Add(..) => Add(first_num, second_num),
+        Subtract(..) => Subtract(first_num, second_num),
+        Multiply(..) => Multiply(first_num, second_num),
+        Divide(..) => Divide(first_num, second_num),
     };
 
     let result = calculate(operation);
